5cdfe76f5be3a6119e4436c7d48123c147d1dac9
oneofthezombies <hunhoekim@gmail.com>
2025-07-31 13:33:19 +0000

    chore: Add C++ configs

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -2,5 +2,26 @@
 
 set -ex
 
+cat /etc/os-release
+uname -a
+
+gcc --version
+g++ --version
+gdb --version
+objcopy --version
+addr2line --version
+gprof --version
+/sbin/ldconfig -p | grep libstdc++
+
+clang --version
+clang++ --version
+lldb --version
+
+cmake --version
+ninja --version
+valgrind --version
+perf --version
+/sbin/ldconfig -p | grep liburing
+
 rustc --version
 cargo --version
