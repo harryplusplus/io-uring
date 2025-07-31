539b0d58dac16a8ce43dd65bd640cb088c87fc91
oneofthezombies <hunhoekim@gmail.com>
2025-07-31 14:26:24 +0000

    feat: Add liburing code

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <liburing.h>
+#include <string.h>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    struct io_uring ring{};
+    int ret = io_uring_queue_init(8, &ring, 0);
+    if (ret < 0) {
+        std::cerr << "io_uring_queue_init failed. code: " << ret << ", " << strerror(-ret) << std::endl;
+        return 1;
+    }
+    std::cout << "io_uring_queue_init succeeded" << std::endl;
+    io_uring_queue_exit(&ring);
     return 0;
 }
